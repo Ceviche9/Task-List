23248fac83d6c5eb07db35c58f7d302439509628
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-05-26 20:47:36 -0300

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,81 @@
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.addBtn');
+const task = document.querySelector('.task');
+
+input.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!input.value) return;
+        createTask(input.value);
+        clearInput();
+    }
+})
+
+function createLi() {
+    const Li = document.createElement('Li');
+    return Li
+}
+
+function createTask(Task) {
+    const Li = createLi();
+    Li.innerText = Task;
+    task.appendChild(Li)
+    btnClear(Li);
+    saveTask();
+}
+
+addBtn.addEventListener('click', function() {
+    if (!input.value) return;
+    createTask(input.value);
+    clearInput();
+  
+});
+
+function clearInput() {
+    input.value = '';
+    input.focus();
+}
+
+function btnClear(Li) {
+
+    Li.innerText += ' ';
+    const btnClear = document.createElement('button');
+    btnClear.innerText = 'Apagar';
+    btnClear.setAttribute('Class', 'Apagar')
+    Li.appendChild(btnClear);
+
+}
+
+document.addEventListener('click',function(e) {
+    const el = e.target;
+    
+    if (el.classList.contains('Apagar')) {
+        el.parentElement.remove();
+        saveTask();   
+    }
+})
+
+function saveTask(){
+    const liTasks = task.querySelectorAll('Li');
+    const List = [];
+    for (let tasks of liTasks) {
+       let taskText = tasks.innerText;
+       taskText = taskText.replace('Apagar', '').trim();
+       List.push(taskText);
+      
+    }
+
+    const taskJson = JSON.stringify(List)
+    localStorage.setItem('Tasks', taskJson);
+
+}
+
+function savedTasks(){
+
+   const tasks = localStorage.getItem('Tasks');
+   const List = JSON.parse(tasks);
+   for (let tasks of List){
+    createTask(tasks);
+   } 
+}
+
+savedTasks();
